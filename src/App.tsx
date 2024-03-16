083e6adf256dc0dc56fd7ba573dc7cb8e31fddde
Sidnei <sidneisonic@hotmail.com>
2024-03-15 22:13:39 -0300

    aula 79 lidando com erros

--- src/App.tsx
+++ src/App.tsx
@@ -10,32 +10,42 @@ type Post = {
 const App = () => {
   const [postsData, setPostsData] = useState<Post[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
 
   const handleGetPosts = async () => {
     setLoading(true);
 
-    const postsRequest = await fetch('https://jsonplaceholder.typicode.com/posts',
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-    // const dataJson = await posts.json();
-    // console.log(dataJson);
+    try {
+      const postsRequest = await fetch('https://jsonplaceholder.typicode.com/posts',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      // const dataJson = await posts.json();
+      // console.log(dataJson);
 
-    const posts: Post[] = await postsRequest.json();
+      const posts: Post[] = await postsRequest.json();
+
+      setPostsData(posts);
+        
+    } catch (error) {
+      setError('Error getting posts');
+    }
 
     setLoading(false);
-
-    setPostsData(posts);
   }
 
   return (
     <div>
       <button onClick={handleGetPosts}>Get Posts</button>
+
       {loading && <p>Loading...</p>}
+      
+      {error && <p>{error}</p>}
+
       <ul>
         {postsData.map((post) => (
           <li key={post.id}>
